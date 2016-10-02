7f850d289587d08e8134b295c5fbe10a2a9b1c37
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2016-10-02 14:39:56 -0400

    Convert to HTML and JavaScript

--- php/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-  <p>&copy; Jessy Lachapelle <?php echo date("Y"); ?><br/>All rights reserved</p>
-</footer>
--- php/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-  <nav>
-    <a href='#about'>About Me</a>
-    <a href='#experience'>Studies and Experience</a>
-    <a href='#projects'>My Projects</a>
-    <a href='#contact'>Contact Me</a>
-  </nav>
-</header>
