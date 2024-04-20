9f9ade054bae1cc5dafc7e55737e765ef6b42348
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2024-04-20 12:40:40 -0700

    Use HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
 
@@ -29,8 +29,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
